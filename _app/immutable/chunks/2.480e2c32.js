3980ce409cd0a253a2ffdc58b39e5d76d6d55b23
Dmitry Parfentev <favoritegrech@yandex.ru>
2023-06-18 02:29:21 +0300

    Updates

--- _app/immutable/chunks/2.480e2c32.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.ebe4474b.js";export{t as component};
---
+++ _app/immutable/chunks/2.e2afb90e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.69de8ff3.js";export{t as component};
